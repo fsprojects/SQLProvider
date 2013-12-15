f71d75f008a2c389131d5170279b55808844cba8
pezi <pezi_pink@pinksquirrellabs.com>
2013-12-15 20:32:55 +0000

    un-fixing build script, changed repo name instead

---
+++ src/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("SQLProvider")>]
+[<assembly: AssemblyProductAttribute("SQLProvider")>]
+[<assembly: AssemblyDescriptionAttribute("Type providers for SQL Server access.")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
