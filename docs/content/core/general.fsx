4fe1565c90494748d06b8cd3a501bf1777c856f5
pezi <pezi_pink@pinksquirrellabs.com>
2014-01-29 16:05:13 +0000

    test to make sure my git is not totally destroyed

--- docs/content/core/general.fsx
+++ docs/content/core/general.fsx
@@ -22,3 +22,5 @@ type sql = SqlDataProvider< connectionString, Common.DatabaseProviderTypes.SQLIT
 let ctx = sql.GetDataContext()
 
 let customers = ctx.``[main].[Customers]`` |> Seq.toArray
+
+let christina = ctx.``[main].[Customers]``.Individuals.``As ContactName``.``BERGS, Christina Berglund``
