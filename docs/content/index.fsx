617943ce2806e84909ee4b02b3693dcdf9db6116
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-29 17:38:32 +0100

    Add line breaks to make the docs a bit nicer

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -7,13 +7,13 @@ SQLProvider
 
 A general SQL database type provider, supporting LINQ queries, schema exploration, individuals and much more besides.
 
-The provider currently supports MS SQL Server, SQLite, PostgreSQL, Oracle and MySQL. All database vendors except SQL Server will require 3rd party ADO.NET connector objects to function.  These are dynamically loaded at runtime so that the SQL provider project is not dependent on them.  You must supply the location of the assemblies with the "ResolutionPath" static parameter.
+The provider currently supports MS SQL Server, SQLite, PostgreSQL, Oracle and MySQL. All database vendors except SQL Server will require 3rd party ADO.NET connector objects to function. These are dynamically loaded at runtime so that the SQL provider project is not dependent on them. You must supply the location of the assemblies with the "ResolutionPath" static parameter.
 
-SQLite is based on the .NET drivers found [here](http://system.data.sqlite.org/index.html/doc/trunk/www/downloads.wiki).  You will need the correct version for your specific architecture and setup.
+SQLite is based on the .NET drivers found [here](http://system.data.sqlite.org/index.html/doc/trunk/www/downloads.wiki). You will need the correct version for your specific architecture and setup.
 
-PostgreSQL is based on the .NET drivers found [here](http://npgsql.projects.pgfoundry.org/).  The type provider will make frequent calls to the database.  I found that using the default settings for the PostgreSQL server on my Windows machine would deny the provider constant access - you may need to try setting  `Pooling=false` in the connection string, increasing timeouts or setting other relevant security settings to enable a frictionless experience.
+PostgreSQL is based on the .NET drivers found [here](http://npgsql.projects.pgfoundry.org/).  The type provider will make frequent calls to the database. I found that using the default settings for the PostgreSQL server on my Windows machine would deny the provider constant access - you may need to try setting  `Pooling=false` in the connection string, increasing timeouts or setting other relevant security settings to enable a frictionless experience.
 
-MySQL is based on the .NET drivers found [here](http://dev.mysql.com/downloads/connector/net/1.0.html).  You will need the correct version for your specific architecture and setup.
+MySQL is based on the .NET drivers found [here](http://dev.mysql.com/downloads/connector/net/1.0.html). You will need the correct version for your specific architecture and setup.
 
 Oracle is based on the current release (12.1.0.1.2) of the managed ODP.NET driver found [here](http://www.oracle.com/technetwork/topics/dotnet/downloads/index.html). However although the managed version is recommended it should also work with previous versions of the native driver.
 
@@ -43,7 +43,12 @@ open FSharp.Data.Sql
 // create a type alias with the connection string and database vendor you are using, along with a resolution path for 
 // 3rd party assemblies if you are not using MS SQL Server.  The last two parameters are the amount of individuals 
 // to project into the devleopment einvronment, and finally whether to generate nullable columns as F# option types.
-type sql = SqlDataProvider< @"Data Source=F:\sqlite\northwindEF.db ;Version=3", Common.DatabaseProviderTypes.SQLITE, @"F:\sqlite\3", 1000, true >
+type sql = SqlDataProvider< 
+              @"Data Source=F:\sqlite\northwindEF.db ;Version=3", 
+              Common.DatabaseProviderTypes.SQLITE, 
+              @"F:\sqlite\3", 
+              1000, 
+              true >
 let ctx = sql.GetDataContext()
 
 // pick individual entities from the database 
