5f2ec42be10a4e17c57c9f40135142978fedafb0
pezi <pezi_pink@pinksquirrellabs.com>
2014-05-14 00:06:57 +0100

    remove tests for now

--- tests/SqlProvider.Tests/General.fs
+++ tests/SqlProvider.Tests/General.fs
@@ -2,20 +2,21 @@
 
 open NUnit.Framework
 
-open FSharp.Data.Sql
-[<Literal>]
-let connectionString = "Data Source=" + __SOURCE_DIRECTORY__ + @"\northwindEF.db;Version=3"
+//open FSharp.Data.Sql
 
-[<Literal>]
-let resolutionPath = __SOURCE_DIRECTORY__ 
-
-type sql = SqlDataProvider<connectionString, Common.DatabaseProviderTypes.SQLITE, resolutionPath >
+//[<Literal>]
+//let connectionString = "Data Source=" + __SOURCE_DIRECTORY__ + @"\northwindEF.db;Version=3"
+//
+//[<Literal>]
+//let resolutionPath = __SOURCE_DIRECTORY__ 
+//
+//type sql = SqlDataProvider<connectionString, Common.DatabaseProviderTypes.SQLITE, resolutionPath >
 
 [<Test>]
 let Test1() = Assert.IsTrue true
 
 
-[<Test>]
-let DbCheck() = 
-    let dc = sql.GetDataContext()
-    Assert.IsTrue true
+//[<Test>]
+//let DbCheck() = 
+//    let dc = sql.GetDataContext()
+//    Assert.IsTrue true
