b38e43342c7f17feb218a5c9254c9112c77e33ca
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-05-22 20:37:55 +0200

    Enable the sqlite test

--- tests/SqlProvider.Tests/General.fs
+++ tests/SqlProvider.Tests/General.fs
@@ -1,22 +1,23 @@
-﻿module SoundCheck
+module SoundCheck
 
 open NUnit.Framework
 
-//open FSharp.Data.Sql
+open FSharp.Data.Sql
 
-//[<Literal>]
-//let connectionString = "Data Source=" + __SOURCE_DIRECTORY__ + @"\northwindEF.db;Version=3"
-//
-//[<Literal>]
-//let resolutionPath = __SOURCE_DIRECTORY__ 
-//
-//type sql = SqlDataProvider<connectionString, Common.DatabaseProviderTypes.SQLITE, resolutionPath >
+[<Literal>]
+let connectionString = @"Data Source=northwindEF.db;Version=3"
+
+[<Literal>]
+let resolutionPath = "../../docs/files/sqlite/"
+
+type sql = SqlDataProvider<connectionString, Common.DatabaseProviderTypes.SQLITE, resolutionPath >
 
 [<Test>]
 let Test1() = Assert.IsTrue true
 
 
-//[<Test>]
-//let DbCheck() = 
-//    let dc = sql.GetDataContext()
-//    Assert.IsTrue true
+[<Test>]
+let DbCheck() = 
+    let dc = sql.GetDataContext()
+    let customers = dc.``[main].[Customers]``
+    Assert.IsTrue true
