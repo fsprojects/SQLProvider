1e4377a2e130851cb6424b2eddf2ba54c59b0a35
Simon Dickson <simonhdickson@gmail.com>
2014-07-27 15:03:51 +0100

    Adding resolution paths to all TPs apart from ODBC and MSSQL

--- src/MSAccessProvider/MSAccessSqlDesignTime.fs
+++ src/MSAccessProvider/MSAccessSqlDesignTime.fs
@@ -17,14 +17,16 @@ type MSAccessProvider(config) as this =
     let conString = ProvidedStaticParameter("ConnectionString",typeof<string>)    
     let optionTypes = ProvidedStaticParameter("UseOptionTypes",typeof<bool>,false)
     let individualsAmount = ProvidedStaticParameter("IndividualsAmount",typeof<int>,1000)
+    let resolutionPath = ProvidedStaticParameter("ResolutionPath",typeof<string>,"")
     let helpText = "<summary>Typed representation of a database</summary>
                     <param name='ConnectionString'>The connection string for the SQL database</param>
                     <param name='IndividualsAmount'>The amount of sample entities to project into the type system for each SQL entity type. Default 1000.</param>
-                    <param name='UseOptionTypes'>If true, F# option types will be used in place of nullable database columns.  If false, you will always receive the default value of the column's type even if it is null in the database.</param>"
+                    <param name='UseOptionTypes'>If true, F# option types will be used in place of nullable database columns.  If false, you will always receive the default value of the column's type even if it is null in the database.</param>
+                    <param name='ResolutionPath'>The location to look for dynamically loaded assemblies containing database vendor specific connections and custom types.</param>"
         
-    do paramSqlType.DefineStaticParameters([conString;individualsAmount;optionTypes], fun typeName args -> 
+    do paramSqlType.DefineStaticParameters([conString;individualsAmount;optionTypes;resolutionPath], fun typeName args -> 
         SqlTypeProvider.createType(args.[0] :?> string,   // OrganizationServiceUrl
-                    "",                                   // Resolution path
+                    args.[3] :?> string,                  // Resolution path
                     args.[1] :?> int,                     // Individuals Amount
                     args.[2] :?> bool,                    // Use option types?
                     "",                                   // Schema owner currently only used for oracle
--- src/OracleProvider/OracleDesignTime.fs
+++ src/OracleProvider/OracleDesignTime.fs
@@ -17,17 +17,21 @@ type OracleProvider(config) as this =
     let conString = ProvidedStaticParameter("ConnectionString",typeof<string>)    
     let optionTypes = ProvidedStaticParameter("UseOptionTypes",typeof<bool>,false)
     let individualsAmount = ProvidedStaticParameter("IndividualsAmount",typeof<int>,1000)
+    let owner = ProvidedStaticParameter("Owner", typeof<string>, "")
+    let resolutionPath = ProvidedStaticParameter("ResolutionPath",typeof<string>,"")   
     let helpText = "<summary>Typed representation of a database</summary>
                     <param name='ConnectionString'>The connection string for the SQL database</param>
                     <param name='IndividualsAmount'>The amount of sample entities to project into the type system for each SQL entity type. Default 1000.</param>
-                    <param name='UseOptionTypes'>If true, F# option types will be used in place of nullable database columns.  If false, you will always receive the default value of the column's type even if it is null in the database.</param>"
+                    <param name='UseOptionTypes'>If true, F# option types will be used in place of nullable database columns.  If false, you will always receive the default value of the column's type even if it is null in the database.</param>
+                    <param name='ResolutionPath'>The location to look for dynamically loaded assemblies containing database vendor specific connections and custom types.</param>
+                    <param name='Owner'>The owner of the schema for this provider to resolve</param>"
         
-    do paramSqlType.DefineStaticParameters([conString;individualsAmount;optionTypes], fun typeName args -> 
+    do paramSqlType.DefineStaticParameters([conString;individualsAmount;optionTypes;resolutionPath;owner], fun typeName args -> 
         SqlTypeProvider.createType(args.[0] :?> string,   // OrganizationServiceUrl
-                    "",                                   // Resolution path
+                    args.[4] :?> string,                  // Resolution path
                     args.[1] :?> int,                     // Individuals Amount
                     args.[2] :?> bool,                    // Use option types?
-                    "",                                   // Schema owner currently only used for oracle
+                    args.[5] :?> string,                  // Schema owner currently only used for oracle
                     DatabaseProviderTypes.ORACLE,
                     sqlRuntimeInfo,
                     ns,
