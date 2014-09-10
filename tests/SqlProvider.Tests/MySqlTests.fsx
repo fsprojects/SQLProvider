143b83d1f276073c76b0f6bc0c48070c15b6c0c2
Colin <colinbul@googlemail.com>
2014-09-10 21:07:59 +0100

    Update mysql tests and fixed a connection not open issue with postgresql

--- tests/SqlProvider.Tests/MySqlTests.fsx
+++ tests/SqlProvider.Tests/MySqlTests.fsx
@@ -23,7 +23,7 @@ type Employee = {
 }
 
 //***************** Individuals ***********************//
-let indv = ctx.``[hr].[employees]``.Individuals.``As FIRST_NAME``.``100, Steven``
+let indv = ctx.``[HR].[EMPLOYEES]``.Individuals.``As FIRST_NAME``.``100, Steven``
 
 indv.FIRST_NAME + " " + indv.LAST_NAME + " " + indv.EMAIL
 
@@ -31,14 +31,14 @@ indv.FIRST_NAME + " " + indv.LAST_NAME + " " + indv.EMAIL
 //*************** QUERY ************************//
 let employeesFirstName = 
     query {
-        for emp in ctx.``[hr].[employees]`` do
+        for emp in ctx.``[HR].[EMPLOYEES]`` do
         select (emp.FIRST_NAME, emp.LAST_NAME)
     } |> Seq.toList
 
 let salesNamedDavid = 
     query {
-            for emp in ctx.``[hr].[employees]`` do
-            join d in ctx.``[hr].[departments]`` on (emp.DEPARTMENT_ID = d.DEPARTMENT_ID)
+            for emp in ctx.``[HR].[EMPLOYEES]`` do
+            join d in ctx.``[HR].[DEPARTMENTS]`` on (emp.DEPARTMENT_ID = d.DEPARTMENT_ID)
             where (d.DEPARTMENT_NAME |=| [|"Sales";"IT"|] && emp.FIRST_NAME =% "David")
             select (d.DEPARTMENT_NAME, emp.FIRST_NAME, emp.LAST_NAME)
             
@@ -46,9 +46,9 @@ let salesNamedDavid =
 
 let employeesJob = 
     query {
-            for emp in ctx.``[hr].[employees]`` do
+            for emp in ctx.``[HR].[EMPLOYEES]`` do
             for manager in emp.employees_ibfk_3 do
-            join dept in ctx.``[hr].[departments]`` on (emp.DEPARTMENT_ID = dept.DEPARTMENT_ID)
+            join dept in ctx.``[HR].[DEPARTMENTS]`` on (emp.DEPARTMENT_ID = dept.DEPARTMENT_ID)
             where ((dept.DEPARTMENT_NAME |=| [|"Sales";"Executive"|]) && emp.FIRST_NAME =% "David")
             select (emp.FIRST_NAME, emp.LAST_NAME, manager.FIRST_NAME, manager.LAST_NAME )
     } |> Seq.toList
@@ -56,7 +56,7 @@ let employeesJob =
 //Can map SQLEntities to a domain type
 let topSales5ByCommission = 
     query {
-        for emp in ctx.``[hr].[employees]`` do
+        for emp in ctx.``[HR].[EMPLOYEES]`` do
         sortByDescending emp.COMMISSION_PCT
         select emp
         take 5
@@ -82,7 +82,7 @@ type Country = {
 //Can customise SQLEntity mapping
 let countries = 
     query {
-        for emp in ctx.``[hr].[countries]`` do
+        for emp in ctx.``[HR].[COUNTRIES]`` do
         select emp
     } 
     |> Seq.map (fun e -> e.MapTo<Country>(fun (prop,value) -> 
@@ -102,14 +102,14 @@ let countries =
 let antartica =
     let result =
         query {
-            for reg in ctx.``[hr].[regions]`` do
+            for reg in ctx.``[HR].[REGIONS]`` do
             where (reg.REGION_ID = 5u)
             select reg
         } |> Seq.toList
     match result with
     | [ant] -> ant
     | _ -> 
-        let newRegion = ctx.``[hr].[regions]``.Create() 
+        let newRegion = ctx.``[HR].[REGIONS]``.Create() 
         newRegion.REGION_NAME <- "Antartica"
         newRegion.REGION_ID <- 5u
         ctx.SubmitUpdates()
