27e26bcb11bfa82df1e7711f065be2979fc671ca
Colin <colinbul@googlemail.com>
2015-03-27 13:50:00 +0000

    Fixed up SQLLite script after merge

--- tests/SqlProvider.Tests/scripts/SQLLiteTests.fsx
+++ tests/SqlProvider.Tests/scripts/SQLLiteTests.fsx
@@ -1,26 +1,26 @@
-﻿#r @"..\..\bin\FSharp.Data.SqlProvider.dll"
+﻿#r @"..\..\..\bin\FSharp.Data.SqlProvider.dll"
 
 open System
 open FSharp.Data.Sql
 
 [<Literal>]
-let connectionString = "Data Source=" + __SOURCE_DIRECTORY__ + @"\northwindEF.db;Version=3"
+let connectionString = @"Data Source=" + __SOURCE_DIRECTORY__ + @"\northwindEF.db;Version=3"
 
 [<Literal>]
-let resolutionPath = __SOURCE_DIRECTORY__ 
+let resolutionPath = __SOURCE_DIRECTORY__ + "\..\libs"
 
 
 open FSharp.Data.Sql
 
 
-type sql = SqlDataProvider<Common.DatabaseProviderTypes.SQLITE, connectionString, ResolutionPath = resolutionPath >
+type sql = SqlDataProvider<Common.DatabaseProviderTypes.SQLITE, connectionString, ResolutionPath = resolutionPath>
 
 let ctx = sql.GetDataContext()
 
-let customers = ctx.``[MAIN].[CUSTOMERS]`` |> Seq.toArray
+let customers = ctx.Main.Customers |> Seq.toArray
 
 let firstCustomer = customers.[0]
-let name = firstCustomer.CONTACTNAME
+let name = firstCustomer.ContactName
 
 let orders = firstCustomer.FK_Orders_0_0 |> Seq.toArray
 
@@ -30,51 +30,51 @@ type Dummy<'t> = D of 't
 
 let employeesFirstName1 = 
     query {
-        for emp in ctx.``[MAIN].[CUSTOMERS]`` do
-        select (D {First=emp.CONTACTNAME})
+        for emp in ctx.Main.Customers do
+        select (D {First=emp.ContactName})
     } |> Seq.toList
 
 let employeesFirstName2 = 
     query {
-        for emp in ctx.``[MAIN].[CUSTOMERS]`` do
-        select ({First=emp.CONTACTNAME} |> D)
+        for emp in ctx.Main.Customers do
+        select ({First=emp.ContactName} |> D)
     } |> Seq.toList
 
 let customersQuery =
-    query { for customer in ctx.``[MAIN].[CUSTOMERS]`` do
+    query { for customer in ctx.Main.Customers do
             select customer } |> Seq.toArray
 
 let filteredQuery =
-    query { for customer in ctx.``[MAIN].[CUSTOMERS]`` do
-            where (customer.CONTACTNAME = "John Smith")
+    query { for customer in ctx.Main.Customers do
+            where (customer.ContactName = "John Smith")
             select customer } |> Seq.toArray
 
 let multipleFilteredQuery =
-    query { for customer in ctx.``[MAIN].[CUSTOMERS]`` do
-            where ((customer.CONTACTNAME = "John Smith" && customer.COUNTRY = "England") 
-                    || customer.CONTACTNAME = "Joe Bloggs")
+    query { for customer in ctx.Main.Customers do
+            where ((customer.ContactName = "John Smith" && customer.Country = "England") 
+                    || customer.ContactName = "Joe Bloggs")
             select customer } |> Seq.toArray
 
 let automaticJoinQuery =
-   query { for customer in ctx.``[MAIN].[CUSTOMERS]`` do
+   query { for customer in ctx.Main.Customers do
            for order in customer.FK_Orders_0_0 do
-           where (customer.CONTACTNAME = "John Smith")
+           where (customer.ContactName = "John Smith")
            select (customer,order) } |> Seq.toArray
 
 let explicitJoinQuery =
-   query { for customer in ctx.``[MAIN].[CUSTOMERS]`` do
-           join order in ctx.``[MAIN].[CUSTOMERS]`` on (customer.CUSTOMERID = order.CUSTOMERID)
-           where (customer.CONTACTNAME = "John Smith")
+   query { for customer in ctx.Main.Customers do
+           join order in ctx.Main.Orders on (customer.CustomerId = order.CustomerId)
+           where (customer.ContactName = "John Smith")
            select (customer,order) } |> Seq.toArray
 
 let ordersQuery =
-   query { for customer in ctx.``[MAIN].[CUSTOMERS]`` do
+   query { for customer in ctx.Main.Customers do
            for order in customer.FK_Orders_0_0 do
-           where (customer.CONTACTNAME = "John Smith")
-           select (customer.CONTACTNAME, order.ORDERDATE, order.SHIPADDRESS) } |> Seq.toArray
+           where (customer.ContactName = "John Smith")
+           select (customer.ContactName, order.OrderDate, order.ShipAddress) } |> Seq.toArray
 
-let BERGS = ctx.``[MAIN].[CUSTOMERS]``.Individuals.BERGS
+let BERGS = ctx.Main.Customers.Individuals.BERGS
 
 
-let christina = ctx.``[MAIN].[CUSTOMERS]``.Individuals.``As CONTACTNAME``.``BERGS, Christina Berglund``
+let christina = ctx.Main.Customers.Individuals.``As ContactName``.``BERGS, Christina Berglund``
 
