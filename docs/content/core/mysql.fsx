3ac144985efb8cdf293b918a6f4ffec739a10930
mclang <Jani.Long@gmail.com>
2016-02-01 10:19:00 +0200

    Syntax and link fixes
    Fixed SqlDataProvider definition and links to MySql .NET Connector and other pages.

--- docs/content/core/mysql.fsx
+++ docs/content/core/mysql.fsx
@@ -16,7 +16,7 @@ open FSharp.Data.Sql
 
 Basic connection string used to connect to MySQL instance; typical connection 
 string parameters apply here.  See 
-(MySQL Connector/NET Connection Strings Documentation)[https://dev.mysql.com/doc/connector-net/en/connector-net-connection-options.html] 
+[MySQL Connector/NET Connection Strings Documentation](https://dev.mysql.com/doc/connector-net/en/connector-net-connection-options.html) 
 for a complete list of connection string options.
 
 *)
@@ -26,19 +26,18 @@ let connString  = "Server=localhost;Database=test;User=test;Password=test"
 (**
 ### ConnectionStringName
 
-Instead of storing the connection string in the source code / `fsx` script, you
-can store values in the `App.config` file.  This is the name of the
-connectionString key/value pair stored in App.config (TODO: confirm file name).
+Instead of storing the connection string in the source code, you
+can store it in the `App.config` file.  This is the name of the
+connectionString key/value pair stored in App.config (TODO: confirm filename).
 *)
 
-// found in App.config (TOOD: confirm)
+// found in App.config (TODO: confirm)
 let connexStringName = "DefaultConnectionString"
 
 (**
 ### Database Vendor
 
-From the `FSharp.Data.Sql.Common.DatabaseProviderTypes` enumeration. For MySQL, 
-use `Common.DatabaseProviderTypes.MYSQL`.
+Use `MYSQL` from the `FSharp.Data.Sql.Common.DatabaseProviderTypes` enumeration.
 
 *)
 
@@ -48,7 +47,7 @@ let dbVendor    = Common.DatabaseProviderTypes.MYSQL
 ### Resolution Path
 
 Path to search for assemblies containing database vendor specific connections and custom types. Type the path where
-`Mysql.Data.dll` is stored.
+`Mysql.Data.dll` is stored. Both absolute and relative paths are supported.
 
 *)
 
@@ -57,8 +56,7 @@ let resPath     = @"C:\Users\JHaas\Documents\Projects\jeroldhaas\CompleteFSharpW
 (**
 ### Individuals Amount
 
-Sets the count to load for each individual. See (individuals)[individuals.html] 
-for further info.
+Sets the count of records to load for each table. See [individuals](individuals.html) for further info.
 
 *)
 
@@ -71,19 +69,27 @@ If true, F# option types will be used in place of nullable database columns.  If
 
 *)
 let useOptTypes = true
-let sql         = SqlDataProvider<
-                    connString,
-                    dbVendor,
-                    resPath,
-                    indivAmount,
-                    useOptTypes
-                 >
-let ctx     = () // TODO: resolve 
+
+(**
+### Example
+
+*)
+
+type sql = SqlDataProvider<
+                connString,
+                dbVendor,
+                resPath,
+                indivAmount,
+                useOptTypes
+            >
+let ctx = () // TODO: resolve
 
 (**
 
 
 ## Caveats / Additional Info
 
-TBD.
+Check [General](general.html), [Static Parameters](parameters.html) and [Querying](querying.html) documentation.
+
+More to be added.
 *)
