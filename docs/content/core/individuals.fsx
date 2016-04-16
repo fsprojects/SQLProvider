b4685f6af8a14ad68cf3f25489179b9e604db2aa
Phillip Trelford <phil@trelford.com>
2016-04-16 15:24:19 +0100

    Added code samples for Individuals feature

--- docs/content/core/individuals.fsx
+++ docs/content/core/individuals.fsx
@@ -1,4 +1,49 @@
-﻿(**
+(*** hide ***)
+#I "../../files/sqlite"
+(*** hide ***)
+#I "../../../bin"
+(*** hide ***)
+#r @"../../../bin/FSharp.Data.SqlProvider.dll"
+
+(*** hide ***)
+[<Literal>]
+let connectionString = "Data Source=" + __SOURCE_DIRECTORY__ + @"/../../../tests/SqlProvider.Tests/scripts/northwindEF.db;Version=3"
+
+(*** hide ***)
+[<Literal>]
+let resolutionPath = __SOURCE_DIRECTORY__ + @"/../../files/sqlite"
+
+(**
 # Individuals
-Documentation stub.
+
+Find individual rows in tables with code completion in the editor.
 *)
+
+open System
+open FSharp.Data.Sql
+
+type sql = SqlDataProvider<Common.DatabaseProviderTypes.SQLITE,
+                           connectionString,
+                           ResolutionPath = resolutionPath,
+                           CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL>
+
+let ctx = sql.GetDataContext()
+
+let customers = ctx.Main.Customers
+
+(**
+Get individual customer row by primary key value
+*)
+customers.Individuals.COMMI
+
+(**
+Get individual customer row using address
+*)
+
+customers.Individuals.``As ContactName``.``COMMI, Pedro Afonso``
+
+(**
+Get individual customer row using address
+*)
+
+customers.Individuals.``As Address``.``CONSH, Berkeley Gardens 12  Brewery``
