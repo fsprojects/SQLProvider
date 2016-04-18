1386f70f55da35dae0ad4f26f08bcfffd7a35fc2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-04-18 12:01:48 +0200

    Update AsemmblyInfo

--- src/SQLProvider/AssemblyInfo.fs
+++ src/SQLProvider/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("SQLProvider")>]
 [<assembly: AssemblyProductAttribute("SQLProvider")>]
 [<assembly: AssemblyDescriptionAttribute("Type providers for SQL Server access.")>]
-[<assembly: AssemblyVersionAttribute("0.0.11")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.11")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.11"
+    let [<Literal>] Version = "1.0.0"
