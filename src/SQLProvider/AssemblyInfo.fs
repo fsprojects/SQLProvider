28fa4bdcd57cd22f4a02916341e52a1c3dd24160
Tim Forkmann <tforkmann@gmail.com>
2016-04-24 21:06:15 +0200

    adding documentation for mssqlserver

--- src/SQLProvider/AssemblyInfo.fs
+++ src/SQLProvider/AssemblyInfo.fs
@@ -3,10 +3,11 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("SQLProvider")>]
 [<assembly: AssemblyProductAttribute("SQLProvider")>]
-[<assembly: AssemblyDescriptionAttribute("Type providers for SQL Server access.")>]
+[<assembly: AssemblyDescriptionAttribute("Type providers for SQL database access.")>]
 [<assembly: AssemblyVersionAttribute("1.0.0")>]
 [<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] Version = "1.0.0"
+    let [<Literal>] InformationalVersion = "1.0.0"
