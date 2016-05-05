4aac9f3acddad319f9abd08a4a471a4f5285a68f
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2016-05-05 15:37:51 +0100

    Minor refactoring for SqlRuntime.Async.fs

--- src/SQLProvider/SqlRuntime.Async.fs
+++ src/SQLProvider/SqlRuntime.Async.fs
@@ -2,11 +2,28 @@
 
 open FSharp.Data.Sql.Runtime
 
-module Seq =
-    let executeQueryAsync = QueryImplementation.executeAsync
 
-module List =
+module Seq =
+    /// Execute SQLProvider query and release the OS thread while query is being executed.
     let executeQueryAsync = QueryImplementation.executeAsync
 
 module Array =
+    /// Execute SQLProvider query and release the OS thread while query is being executed.
     let executeQueryAsync = QueryImplementation.executeAsync
+
+module List =
+    /// Execute SQLProvider query and release the OS thread while query is being executed.
+    let executeQueryAsync = QueryImplementation.executeAsync
+
+    /// Helper function to run async computation non-parallel style for list of objects.
+    /// This is needed if async database opreation is executed for a list of entities.
+    let evaluateOneByOne asyncFunc entityList =
+        let rec executeOneByOne' asyncFunc entityList acc =
+            match entityList with
+            | [] -> async { return acc }
+            | h::t -> 
+                async {
+                    let! res = asyncFunc h
+                    return! executeOneByOne' asyncFunc t (res::acc)
+                }
+        executeOneByOne' asyncFunc entityList []
