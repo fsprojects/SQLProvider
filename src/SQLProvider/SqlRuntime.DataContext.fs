cfd72f9dcdc0d3f3ebba32495ab19108bdbfe1d3
Janno Põldma <janno.poldma@gmail.com>
2016-05-06 11:59:46 +0300

    Refactor SqlDataContext type.

--- src/SQLProvider/SqlRuntime.DataContext.fs
+++ src/SQLProvider/SqlRuntime.DataContext.fs
@@ -26,11 +26,12 @@ module internal ProviderBuilder =
 type public SqlDataContext (typeName,connectionString:string,providerType,resolutionPath, referencedAssemblies, runtimeAssembly, owner, caseSensitivity) =
     let pendingChanges = HashSet<SqlEntity>()
     static let providerCache = Dictionary<string,ISqlProvider>()
-    do
+
+    let provider =
         lock providerCache (fun () ->
             match providerCache .TryGetValue typeName with
-            | true, _ -> ()
-            | false,_ ->
+            | true, prov -> prov
+            | _ ->
                 let prov = ProviderBuilder.createProvider providerType resolutionPath referencedAssemblies runtimeAssembly owner
                 use con = prov.CreateConnection(connectionString)
                 con.Open()
@@ -39,130 +40,107 @@ type public SqlDataContext (typeName,connectionString:string,providerType,resolu
                 prov.CreateTypeMappings(con)
                 prov.GetTables(con,caseSensitivity) |> ignore
                 if (providerType.GetType() <> typeof<Providers.MSAccessProvider>) then con.Close()
-                providerCache.Add(typeName,prov))
+                providerCache.Add(typeName,prov)
+                prov)
 
     interface ISqlDataContext with
         member __.ConnectionString with get() = connectionString
-
-        member __.CreateConnection() =
-            match providerCache.TryGetValue typeName with
-            | true,provider -> provider.CreateConnection(connectionString)
-            | false, _ -> failwith "fatal error - provider cache was not populated with expected ISqlprovider instance"
+        member __.CreateConnection() = provider.CreateConnection(connectionString)
 
         member __.GetPrimaryKeyDefinition(tableName) =
-            match providerCache.TryGetValue typeName with
-            | true,provider ->
-                use con = provider.CreateConnection(connectionString)
-                provider.GetTables(con, caseSensitivity)
-                |> List.tryFind (fun t -> t.Name = tableName)
-                |> Option.bind (fun t -> provider.GetPrimaryKey(t))
-                |> (fun x -> defaultArg x "")
-            | false, _ -> failwith "fatal error - provider cache was not populated with expected ISqlprovider instance"
+            use con = provider.CreateConnection(connectionString)
+            provider.GetTables(con, caseSensitivity)
+            |> List.tryFind (fun t -> t.Name = tableName)
+            |> Option.bind (fun t -> provider.GetPrimaryKey(t))
+            |> (fun x -> defaultArg x "")
 
         member __.SubmitChangedEntity e = pendingChanges.Add e |> ignore
         member __.ClearPendingChanges() = pendingChanges.Clear()
         member __.GetPendingEntities() = pendingChanges |> Seq.toList
 
         member __.SubmitPendingChanges() =
-            match providerCache.TryGetValue typeName with
-            | true,provider ->
-                use con = provider.CreateConnection(connectionString)
-                provider.ProcessUpdates(con, Seq.toList pendingChanges)
-                pendingChanges.Clear()
-            | false, _ -> failwith "fatal error - provider cache was not populated with expected ISqlprovider instance"
+            use con = provider.CreateConnection(connectionString)
+            provider.ProcessUpdates(con, Seq.toList pendingChanges)
+            pendingChanges.Clear()
 
         member __.SubmitPendingChangesAsync() =
-            match providerCache.TryGetValue typeName with
-            | true,provider ->
-                async {
-                    use con = provider.CreateConnection(connectionString) :?> System.Data.Common.DbConnection
-                    do! provider.ProcessUpdatesAsync(con, Seq.toList pendingChanges)
-                    pendingChanges.Clear()
-                }
-            | false, _ -> failwith "fatal error - provider cache was not populated with expected ISqlprovider instance"
+            async {
+                use con = provider.CreateConnection(connectionString) :?> System.Data.Common.DbConnection
+                do! provider.ProcessUpdatesAsync(con, Seq.toList pendingChanges)
+                pendingChanges.Clear()
+            }
 
         member this.CreateRelated(inst:SqlEntity,_,pe,pk,fe,fk,direction) : IQueryable<SqlEntity> =
-            match providerCache.TryGetValue typeName with
-            | true,provider ->
-               if direction = RelationshipDirection.Children then
-                   QueryImplementation.SqlQueryable<_>(this,provider,
-                      FilterClause(
-                         Condition.And(["__base__",fk,ConditionOperator.Equal, Some(inst.GetColumn pk)],None),
-                            BaseTable("__base__",Table.FromFullName fe)),ResizeArray<_>()) :> IQueryable<_>
-               else
-                   QueryImplementation.SqlQueryable<_>(this,provider,
-                      FilterClause(
-                         Condition.And(["__base__",pk,ConditionOperator.Equal, Some(box<|inst.GetColumn fk)],None),
-                            BaseTable("__base__",Table.FromFullName pe)),ResizeArray<_>()) :> IQueryable<_>
-             | false, _ -> failwith "fatal error - provider cache was not populated with expected ISqlprovider instance"
+            if direction = RelationshipDirection.Children then
+                QueryImplementation.SqlQueryable<_>(this,provider,
+                    FilterClause(
+                        Condition.And(["__base__",fk,ConditionOperator.Equal, Some(inst.GetColumn pk)],None),
+                        BaseTable("__base__",Table.FromFullName fe)),ResizeArray<_>()) :> IQueryable<_>
+            else
+                QueryImplementation.SqlQueryable<_>(this,provider,
+                    FilterClause(
+                        Condition.And(["__base__",pk,ConditionOperator.Equal, Some(box<|inst.GetColumn fk)],None),
+                        BaseTable("__base__",Table.FromFullName pe)),ResizeArray<_>()) :> IQueryable<_>
 
         member this.CreateEntities(table:string) : IQueryable<SqlEntity> =
-            match providerCache.TryGetValue typeName with
-            | true,provider -> QueryImplementation.SqlQueryable.Create(Table.FromFullName table,this,provider)
-            | false, _ -> failwith "fatal error - provider cache was not populated with expected ISqlprovider instance"
+            QueryImplementation.SqlQueryable.Create(Table.FromFullName table,this,provider)
 
         member this.CallSproc(def:RunTimeSprocDefinition, retCols:QueryParameter[], values:obj array) =
-            match providerCache.TryGetValue typeName with
-            | true,provider ->
-               use con = provider.CreateConnection(connectionString)
-               con.Open()
-               use com = provider.CreateCommand(con, def.Name.DbName)
-               com.CommandType <- CommandType.StoredProcedure
+            use con = provider.CreateConnection(connectionString)
+            con.Open()
+            use com = provider.CreateCommand(con, def.Name.DbName)
+            com.CommandType <- CommandType.StoredProcedure
 
-               let entity = new SqlEntity(this, def.Name.DbName)
+            let entity = new SqlEntity(this, def.Name.DbName)
 
-               let toEntityArray rowSet =
-                   [|
-                       for row in rowSet do
-                           let entity = new SqlEntity(this, def.Name.DbName)
-                           entity.SetData(row)
-                           yield entity
-                   |]
+            let toEntityArray rowSet =
+                [|
+                    for row in rowSet do
+                        let entity = new SqlEntity(this, def.Name.DbName)
+                        entity.SetData(row)
+                        yield entity
+                |]
 
-               let param = def.Params |> List.toArray
+            let param = def.Params |> List.toArray
 
-               Common.QueryEvents.PublishSqlQuery (sprintf "EXEC %s(%s)" com.CommandText (String.Join(", ", (values |> Seq.map (sprintf "%A")))))
+            Common.QueryEvents.PublishSqlQuery (sprintf "EXEC %s(%s)" com.CommandText (String.Join(", ", (values |> Seq.map (sprintf "%A")))))
 
-               let entities =
-                   match provider.ExecuteSprocCommand(com, param, retCols, values) with
-                   | Unit -> () |> box
-                   | Scalar(name, o) -> entity.SetColumnSilent(name, o); entity |> box
-                   | SingleResultSet(name, rs) -> entity.SetColumnSilent(name, toEntityArray rs); entity |> box
-                   | Set(rowSet) ->
-                       for row in rowSet do
-                            match row with
-                            | ScalarResultSet(name, o) -> entity.SetColumnSilent(name, o);
-                            | ResultSet(name, rs) ->
-                                let data = toEntityArray rs
-                                entity.SetColumnSilent(name, data)
-                       entity |> box
+            let entities =
+                match provider.ExecuteSprocCommand(com, param, retCols, values) with
+                | Unit -> () |> box
+                | Scalar(name, o) -> entity.SetColumnSilent(name, o); entity |> box
+                | SingleResultSet(name, rs) -> entity.SetColumnSilent(name, toEntityArray rs); entity |> box
+                | Set(rowSet) ->
+                    for row in rowSet do
+                        match row with
+                        | ScalarResultSet(name, o) -> entity.SetColumnSilent(name, o);
+                        | ResultSet(name, rs) ->
+                            let data = toEntityArray rs
+                            entity.SetColumnSilent(name, data)
+                    entity |> box
 
-               if (provider.GetType() <> typeof<Providers.MSAccessProvider>) then con.Close()
-               entities
-            | false, _ -> failwith "fatal error - provider cache was not populated with expected ISqlprovider instance"
+            if (provider.GetType() <> typeof<Providers.MSAccessProvider>) then con.Close()
+            entities
 
         member this.GetIndividual(table,id) : SqlEntity =
-            match providerCache.TryGetValue typeName with
-            | true,provider ->
-               use con = provider.CreateConnection(connectionString)
-               con.Open()
-               let table = Table.FromFullName table
-               // this line is to ensure the columns for the table have been retrieved and therefore
-               // its primary key exists in the lookup
-               lock provider (fun () -> provider.GetColumns (con,table) |> ignore)
-               let pk =
-                   match provider.GetPrimaryKey table with
-                   | Some v -> v
-                   | None ->
-                      // this fail case should not really be possible unless the runtime database is different to the design-time one
-                      failwithf "Primary key could not be found on object %s. Individuals only supported on objects with a single primary key." table.FullName
+            use con = provider.CreateConnection(connectionString)
+            con.Open()
+            let table = Table.FromFullName table
+            // this line is to ensure the columns for the table have been retrieved and therefore
+            // its primary key exists in the lookup
+            lock provider (fun () -> provider.GetColumns (con,table) |> ignore)
+            let pk =
+                match provider.GetPrimaryKey table with
+                | Some v -> v
+                | None ->
+                    // this fail case should not really be possible unless the runtime database is different to the design-time one
+                    failwithf "Primary key could not be found on object %s. Individuals only supported on objects with a single primary key." table.FullName
 
-               use com = provider.CreateCommand(con,provider.GetIndividualQueryText(table,pk))
-               //todo: establish pk SQL data type
-               com.Parameters.Add (provider.CreateCommandParameter(QueryParameter.Create("@id", 0),id)) |> ignore
-               if con.State <> ConnectionState.Open then con.Open()
-               use reader = com.ExecuteReader()
-               let entity = SqlEntity.FromDataReader(this,table.FullName,reader).[0]
-               if (provider.GetType() <> typeof<Providers.MSAccessProvider>) then con.Close()
-               entity
-            | false, _ -> failwith "fatal error - connection cache was not populated with expected connection details"
+            use com = provider.CreateCommand(con,provider.GetIndividualQueryText(table,pk))
+            //todo: establish pk SQL data type
+            com.Parameters.Add (provider.CreateCommandParameter(QueryParameter.Create("@id", 0),id)) |> ignore
+            if con.State <> ConnectionState.Open then con.Open()
+            use reader = com.ExecuteReader()
+            let entity = SqlEntity.FromDataReader(this,table.FullName,reader).[0]
+            if (provider.GetType() <> typeof<Providers.MSAccessProvider>) then con.Close()
+            entity
