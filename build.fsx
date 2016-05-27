e7c2d41692be2a72d2da48db71e537d26367ffe9
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2016-05-27 14:28:10 +0100

    test Travis CI unit test ability.

--- build.fsx
+++ build.fsx
@@ -236,7 +236,7 @@ Target "BuildDocs" DoNothing
 "Clean"
   ==> "AssemblyInfo"
   ==> "Build"
-  =?> ("RunTests", isLocalBuild || not isMono) //AppVeyor can't run database tests
+  ==> "RunTests" //Travis can't run database tests
   ==> "CleanDocs"
   =?> ("GenerateReferenceDocs",isLocalBuild && not isMono)
   =?> ("GenerateHelp",isLocalBuild && not isMono)
@@ -246,7 +246,7 @@ Target "BuildDocs" DoNothing
   ==> "BuildDocs"
 
 "All" 
-  ==> "NuGet"
+//  ==> "NuGet"
   ==> "ReleaseDocs"
   ==> "Release"
 
