1fd6f028f93722ae24f57dabffe358e271d9ab8b
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2016-05-27 14:58:03 +0100

    1.0.7

--- src/SQLProvider/AssemblyInfo.fs
+++ src/SQLProvider/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("SQLProvider")>]
 [<assembly: AssemblyProductAttribute("SQLProvider")>]
 [<assembly: AssemblyDescriptionAttribute("Type providers for SQL database access.")>]
-[<assembly: AssemblyVersionAttribute("1.0.6")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.6")>]
+[<assembly: AssemblyVersionAttribute("1.0.7")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.6"
-    let [<Literal>] InformationalVersion = "1.0.6"
+    let [<Literal>] Version = "1.0.7"
+    let [<Literal>] InformationalVersion = "1.0.7"
