0b4e48963654db1f590f7a7fbd34c95d0e4f53b1
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2016-06-01 15:49:09 +0100

    readme update

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -126,12 +126,14 @@ The project is hosted on [GitHub][gh] where you can [report issues][issues], for
 the project and submit pull requests. If you're adding new public API, please also 
 consider adding [samples][content] that can be turned into a documentation. You might
 also want to read [library design notes][readme] to understand how it works.
+Our tests have [more samples][tests].
 
 The library is available under Public Domain license, which allows modification and 
 redistribution for both commercial and non-commercial purposes. For more information see the 
 [License file][license] in the GitHub repository. 
 
-  [content]: https://github.com/fsprojects/SQLProvider/tree/master/docs/content
+  [content]: https://github.com/fsprojects/SQLProvider/tree/master/docs/content/core
+  [tests]: https://github.com/fsprojects/SQLProvider/tree/master/tests/SqlProvider.Tests/scripts
   [gh]: https://github.com/fsprojects/SQLProvider
   [issues]: https://github.com/fsprojects/SQLProvider/issues
   [readme]: https://github.com/fsprojects/SQLProvider/blob/master/README.md
