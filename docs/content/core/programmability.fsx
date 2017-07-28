08a03f0c570401d50ec6a06c8656fe24351c7098
Tuomas Hietanen <thorium@iki.fi>
2017-07-28 03:09:40 +0100

    more docs for procedures

--- docs/content/core/programmability.fsx
+++ docs/content/core/programmability.fsx
@@ -1,18 +1,55 @@
+(*** hide ***)
+#I @"../../../bin"
+(*** hide ***)
+#r @"../../../bin/FSharp.Data.SqlProvider.dll"
+(*** hide ***)
+open System
+(*** hide ***)
+open FSharp.Data.Sql
+(*** hide ***)
+[<Literal>]
+let connStr = @"Data Source=localhost; Initial Catalog=AdventureWorks2014; Integrated Security=True"
 (**
 # Programmability
 *)
 
-type AdventureWorks = SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, connStr, ResolutionPath = resolutionFolder>
+type AdventureWorks = SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, connStr>
 let ctx = AdventureWorks.GetDataContext()
 
 (**
+
+Querying views is just like querying tables. But executing a stored procedure or a function is a bit different:
+
 Execute a function in the Adventure Works database
+
 *)
 
 ctx.Functions.UfnGetSalesOrderStatusText.Invoke(0uy)
 
 (**
+
 Execute a stored procedure in the Adventure Works database
+
 *)
 
 ctx.Procedures.UspLogError.Invoke(1)
+
+(**
+
+Example of executing a procedure with async and reading the results:
+
+*)
+
+let uspGetManagerEmployees =
+    async {
+        let! res = ctx.Procedures.UspGetManagerEmployees.InvokeAsync 2
+        let mapped = res.ResultSet |> Array.map(fun i -> i.ColumnValues |> Map.ofSeq)
+        mapped |> Array.iter(fun i -> 
+            printfn "Name: %O, Level: %O" i.["FirstName"] i.["RecursionLevel"]
+        )
+    } |> Async.StartAsTask
+
+    // Name: Roberto, Level: 1
+    // Name: Rob, Level: 2
+    // Name: Gail, Level: 2
+    // ...
