0719bde1d2fd308c9213c066f06e346ece52204b
Tuomas Hietanen <thorium@iki.fi>
2017-10-22 14:01:40 +0100

    .Net Core tests update

---
+++ tests/SqlProvider.Core.Tests/MySql/build.sh
@@ -0,0 +1,8 @@
+# May need sudo on Linux: sh ./build.sh
+mono ..\..\..\.paket\paket.bootstrapper.exe
+mono ..\..\..\.paket\paket.exe restore
+rm -r -f obj
+rm -r -f bin
+dotnet restore
+dotnet build
+dotnet run
--- tests/SqlProvider.Core.Tests/SQLite/build.cmd
+++ tests/SqlProvider.Core.Tests/SQLite/build.cmd
@@ -1,3 +1,5 @@
+..\..\..\.paket\paket.bootstrapper.exe
+..\..\..\.paket\paket.exe restore
 rd /s /q obj
 rd /s /q bin
 dotnet restore
