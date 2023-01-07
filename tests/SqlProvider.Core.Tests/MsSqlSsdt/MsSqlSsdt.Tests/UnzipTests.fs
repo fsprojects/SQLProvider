e79d0d6c8fcf6ec7e70116cf6ec0ecc45efb0873
Jordan Marr <JordanMarr@users.noreply.github.com>
2023-01-07 13:59:04 -0500

    Added MsSqlSsdt.Tests.fsproj back (fixed build issues).

--- tests/SqlProvider.Core.Tests/MsSqlSsdt/MsSqlSsdt.Tests/UnzipTests.fs
+++ tests/SqlProvider.Core.Tests/MsSqlSsdt/MsSqlSsdt.Tests/UnzipTests.fs
@@ -1,14 +1,10 @@
-module Dacpac.UnzipTests
+module MsSqlSsdt.Tests.UnzipTests
 open NUnit.Framework
 open System.IO.Compression
 open System
 
 [<Literal>]
-let projDir = __SOURCE_DIRECTORY__
-
-let dacPacPath =
-    let dir = IO.DirectoryInfo(projDir).Parent.FullName
-    IO.Path.Combine(dir, @"AdventureWorks_SSDT\bin\Debug\AdventureWorks_SSDT.dacpac")
+let dacPacPath = __SOURCE_DIRECTORY__ + "/AdventureWorks_SSDT.dacpac"
 
 let extractModelXml(path: string) = 
     use stream = new IO.FileStream(path, IO.FileMode.Open)
