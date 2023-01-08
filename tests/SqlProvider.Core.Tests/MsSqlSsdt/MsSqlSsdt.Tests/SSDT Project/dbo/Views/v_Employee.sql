52782a2eabd201ff051f1c36c26e1654d1215816
Jordan Marr <JordanMarr@users.noreply.github.com>
2023-01-08 14:45:05 -0500

    Removed unused SSDT Project

--- tests/SqlProvider.Core.Tests/MsSqlSsdt/MsSqlSsdt.Tests/SSDT Project/dbo/Views/v_Employee.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW [dbo].[v_Employee]
-    AS  
-    SELECT EmployeeDetails.EmpId, EmpName, EmployeeDetails.EmpSalary, EmployeeContact.MobileNo as MobilePhone
-    FROM [dbo].EmployeeDetails   
-    LEFT OUTER JOIN [dbo].EmployeeContact ON
-    dbo.EmployeeDetails.Emp_Id = dbo.EmployeeContact.EmpId
-    WHERE dbo.EmployeeDetails.EmpId > 2
--- tests/SqlProvider.Core.Tests/MsSqlSsdt/MsSqlSsdt.Tests/UnzipTests.fs
+++ tests/SqlProvider.Core.Tests/MsSqlSsdt/MsSqlSsdt.Tests/UnzipTests.fs
@@ -4,7 +4,7 @@ open System.IO.Compression
 open System
 
 [<Literal>]
-let dacPacPath = __SOURCE_DIRECTORY__ + "/AdventureWorks_SSDT.dacpac"
+let dacPacPath = __SOURCE_DIRECTORY__ + "/AdventureWorks_SSDT/bin/Debug/AdventureWorks_SSDT.dacpac"
 
 let extractModelXml(path: string) = 
     use stream = new IO.FileStream(path, IO.FileMode.Open)
