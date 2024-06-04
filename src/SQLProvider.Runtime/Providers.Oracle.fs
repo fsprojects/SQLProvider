c34269ca66f2f41d9fae600807c1b9ff314c7f2c
Brandon Williams <willibrandon@gmail.com>
2024-06-03 22:09:34 -0700

    Detect Oracle virtual columns using all_tab_cols

--- src/SQLProvider.Runtime/Providers.Oracle.fs
+++ src/SQLProvider.Runtime/Providers.Oracle.fs
@@ -270,7 +270,7 @@ module internal Oracle =
         |> Seq.toList
 
     let getColumns (primaryKeys:IDictionary<_,_>) (table : Table) conn = 
-        sprintf """select data_type, nullable, column_name, data_length, data_default, virtual_column from all_tab_columns where table_name = '%s' and owner = '%s'""" table.Name table.Schema
+        sprintf """select data_type, nullable, column_name, data_length, data_default, virtual_column from all_tab_cols where table_name = '%s' and owner = '%s'""" table.Name table.Schema
         |> read conn (fun row ->
                 let columnType : string = Sql.dbUnbox row.[0]
                 // Remove precision specification from the column type name (can appear in TIMESTAMP and INTERVAL)
