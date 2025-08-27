f55e4c649ed4a406e6a80b743145b483c82a016d
Tom <artemsloboda@gmail.com>
2025-08-27 06:05:50 +0100

    Checking paths

--- build.fsx
+++ build.fsx
@@ -3,7 +3,10 @@ printfn "Building..."
 
 #if FAKE
 #r "paket: groupref build //"
-#r "../../.nuget/packages/build/System.Data.SqlClient/lib/netstandard2.0/System.Data.SqlClient.dll"
+#r "./.nuget/packages/build/System.Data.SqlClient/lib/netstandard2.0/System.Data.SqlClient.dll"
+#r "./../.nuget/packages/build/System.Data.SqlClient/lib/netstandard2.0/System.Data.SqlClient.dll"
+#r "./../../.nuget/packages/build/System.Data.SqlClient/lib/netstandard2.0/System.Data.SqlClient.dll"
+#r "./../../../.nuget/packages/build/System.Data.SqlClient/lib/netstandard2.0/System.Data.SqlClient.dll"
 open System.Data.SqlClient
 #endif
 
